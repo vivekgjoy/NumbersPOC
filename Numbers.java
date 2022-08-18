f7d917be4694362d02eb981da9d06d3946eafae7
vivekplato <109714586+vivekplato@users.noreply.github.com>
2022-08-18 17:09:32 +0530

    NumbersPOC

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers
+{
+	public static void main(String []args)
+	{
+		for (int i=1; i<=20; i++)
+		{
+			for (int j=1; j<=12; j++)
+			{
+				System.out.println(i+" "+"x"+" "+j+" "+"="+(i*j));
+			}
+			System.out.println("************************************************");
+		}
+
+	}
+
+}
